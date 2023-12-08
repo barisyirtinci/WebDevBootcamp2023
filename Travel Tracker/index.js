30533aad6f64cd99300d7a7985532a5d0c964ba6
barisyirtinci <brsyrtnc@gmail.com>
2023-12-09 02:56:08 +0300

    TravelTracker stage 2/4 completed

--- Travel Tracker/index.js
+++ Travel Tracker/index.js
@@ -18,15 +18,32 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+async function checkVisited(){
+  const result = await db.query("SELECT * FROM visited_countries");
+  
   let countries = [];
-  result.rows.forEach((country) => {
+  result.rows.forEach((country)=>{
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
   res.render("index.ejs", {countries: countries, total: countries.length});
-  db.end();
+  //db.end();
+});
+
+app.post("/add", async(req,res)=>{
+  const input = req.body["country"];
+  const result = await db.query("SELECT * from countries WHERE country_name = $1", [input]);
+
+  if(result.rows.length !==0){
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
